22e7eb9af6fa198ede31e9ce46fcd05f4f73bd9f
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2022-12-19 16:07:25 -0300

    criando comentários dos métodos da lib

--- core/src/main/java/com/network/scanner/core/scanner/NetScan.kt
+++ core/src/main/java/com/network/scanner/core/scanner/NetScan.kt
@@ -9,25 +9,69 @@ import com.network.scanner.core.scanner.tools.portscan.PortScanResult
 
 interface NetScan {
 
+    /**
+     * This method is used to understand if a specific device is reachable in the current network,
+     * using the Java ICMP layer.
+     * @param host It is the hostname that you want to find.
+     * @return NetScanObservable<PingResult> You can use the methods, onResult() and onError()
+     * to observe the ping response.
+     * */
     @RequiresApi(Build.VERSION_CODES.M)
     fun pingByIcmp(host: String): NetScanObservable<PingResult>
 
+
+    /**
+     * This method is used to check if the device has wifi connection.
+     * @return Will return true is returned if has connection and false if has not.
+     * */
     @RequiresApi(value = Build.VERSION_CODES.M)
     fun hasWifiConnection(): Boolean
 
+    /**
+     * This method is used to check if the device has cellular connection.
+     * @return Will return true is returned if has connection and false if has not.
+     * */
     @RequiresApi(value = Build.VERSION_CODES.M)
     fun hasCellularConnection(): Boolean
 
+    /**
+     * This method is used to check if the device has ethernet connection.
+     * @return Will return true is returned if has connection and false if has not.
+     * */
     @RequiresApi(value = Build.VERSION_CODES.M)
     fun hasEthernetConnection(): Boolean
 
+    /**
+     * This method is used to check if the device has some connection.
+     * The connection can be from wifi, ethernet or cellular.
+     * @return Will return true is returned if has connection and false if has not.
+     * */
     @RequiresApi(value = Build.VERSION_CODES.M)
     fun hasSomeConnection(): Boolean
 
+    /**
+     * This method is used to check if the device has internet connection.
+     * @return Will return true is returned if has connection and false if has not.
+     * */
     fun hasInternetConnection(): Boolean
 
+    /**
+     * This method is used to understand if a specific device is reachable in the current network,
+     * using the "ping" unix command.
+     * @param host It is the hostname that you want to find.
+     * @return NetScanObservable<PingResult> You can use the methods, onResult() and onError()
+     * to observe the ping response.
+     * */
     fun pingBySystem(host: String): NetScanObservable<PingResult>
 
+    /**
+     * This method is used to understand if a specific device port is opened.
+     * @param ipAddress It is the ipAddress from the device to check.
+     * @param port It is the port to check.
+     * @param timeout It is the timeout to check if the port is opened.
+     * @return NetScanObservable<PortScanResult> You can use the methods, onResult() and onError()
+     * to observe the ping response.
+     * */
     fun portScan(ipAddress: String, port: Int, timeout: Int): NetScanObservable<PortScanResult>
 
     companion object {
